61151044263a2c8db8362f5c20ec4082888ce6e0
MonishGiri <monishgiri46@gmail.com>
2024-07-22 07:53:28 +0530

    Created all entities

---
+++ src/main/java/com/scm/entities/User.java
@@ -0,0 +1,58 @@
+package com.scm.entities;
+
+import java.util.ArrayList;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "users")
+public class User {
+
+    @Id
+    private String userId;
+
+    @Column(nullable = false)
+    private String userName;
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+    @Column(length = 10000)
+    private String about;
+    @Column(length = 10000)
+    private String profilePic;
+
+    private String phoneNumber;
+
+    private boolean enabled = false;
+
+    private boolean emailVerified = false;
+
+    private boolean phoneVerified = false;
+
+    // Self, Google, facebook, linkedin , twitter
+    private Providers provider=Providers.SELF;
+
+    private String providerUserId;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Contact> contacts = new ArrayList<>();
+
+}
